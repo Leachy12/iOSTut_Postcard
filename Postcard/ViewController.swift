97e7cd133f8f0c25f933828975d0ec9f5092e2b1
Sean Leach <seanl@thisisbamboo.com>
2014-10-17 14:58:09 +0100

    First Rev

--- Postcard/ViewController.swift
+++ Postcard/ViewController.swift
@@ -10,6 +10,10 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var messageLabel: UILabel!
+    @IBOutlet weak var enterNameTextField: UITextField!
+    @IBOutlet weak var enterMessageTextField: UITextField!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
@@ -20,6 +24,13 @@ class ViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
 
+    @IBAction func sendButtonWasPressed(sender: UIButton) {
+        messageLabel.text="Hello, " + enterNameTextField.text + "!\n"+enterMessageTextField.text
+        messageLabel.hidden = false
+        enterMessageTextField.text = ""
+        enterNameTextField.text = ""
+        enterMessageTextField.resignFirstResponder()
+    }
 
 }
 
